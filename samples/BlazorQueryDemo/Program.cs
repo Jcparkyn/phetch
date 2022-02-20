3666c355ef62cec5164e91f8f64b2d6897bc1d21
Jcparkyn <51850908+Jcparkyn@users.noreply.github.com>
2022-02-20 12:34:32 +1100

    Switch demo project to MudBlazor

--- samples/BlazorQueryDemo/Program.cs
+++ samples/BlazorQueryDemo/Program.cs
@@ -1,11 +1,13 @@
-using BlazorQueryDemo;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorQueryDemo;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
