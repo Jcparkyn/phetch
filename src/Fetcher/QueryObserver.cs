db4bf10ac92c8e7cfd40ea0b0a27cf4520c0d73f
Jcparkyn <51850908+Jcparkyn@users.noreply.github.com>
2022-03-18 14:56:45 +1100

    Begin migrating demo pages to QueryObserver

--- src/Fetcher/QueryObserver.cs
+++ src/Fetcher/QueryObserver.cs
@@ -20,15 +20,22 @@ public class QueryObserver<TArg, TResult> : IQueryObserver<TResult>
 {
     private readonly QueryCache<TArg, TResult> _cache;
     private readonly QueryObserverOptions<TResult> _options;
+    private FixedQuery<TResult>? _lastSuccessfulQuery;
     private FixedQuery<TResult>? _currentQuery;
 
     public event Action StateChanged = delegate { };
 
     public QueryStatus Status => _currentQuery?.Status ?? QueryStatus.Idle;
 
-    public TResult? Data => _currentQuery is null
-        ? default
-        : _currentQuery.Data;
+    public TResult? Data => _currentQuery is not null
+        ? _currentQuery.Data
+        : default;
+
+    public TResult? LastData => IsSuccess
+        ? _currentQuery.Data
+        : _lastSuccessfulQuery?.Status == QueryStatus.Success
+            ? _lastSuccessfulQuery.Data
+            : default;
 
     public Exception? Error => _currentQuery?.Error;
 
@@ -37,10 +44,11 @@ public class QueryObserver<TArg, TResult> : IQueryObserver<TResult>
     [MemberNotNullWhen(true, nameof(Error))]
     public bool IsError => Status == QueryStatus.Error;
 
+    [MemberNotNullWhen(true, nameof(_currentQuery))]
     public bool IsSuccess => _currentQuery?.Status == QueryStatus.Success;
 
     [MemberNotNullWhen(true, nameof(Data))]
-    public bool HasData => Data is not null;
+    public bool HasData => Data is not null; // TODO: support value types
 
     public bool IsUninitialized => Status == QueryStatus.Idle;
 
@@ -48,15 +56,15 @@ public class QueryObserver<TArg, TResult> : IQueryObserver<TResult>
 
     public QueryObserver(
         QueryCache<TArg, TResult> cache,
-        QueryObserverOptions<TResult> options)
+        QueryObserverOptions<TResult>? options = null)
     {
         _cache = cache;
-        _options = options;
+        _options = options ?? new();
     }
 
     public QueryObserver(
         Func<TArg, Task<TResult>> queryFn,
-        QueryObserverOptions<TResult> options
+        QueryObserverOptions<TResult>? options = null
     ) : this(new QueryCache<TArg, TResult>(queryFn, null), options) { }
 
     public void Refetch()
@@ -92,6 +100,7 @@ public class QueryObserver<TArg, TResult> : IQueryObserver<TResult>
         switch (e)
         {
             case QueryEvent.Success:
+                _lastSuccessfulQuery = _currentQuery;
                 _options.OnSuccess?.Invoke(result);
                 break;
             case QueryEvent.Error:
@@ -99,5 +108,15 @@ public class QueryObserver<TArg, TResult> : IQueryObserver<TResult>
                 break;
         }
         StateChanged?.Invoke();
+    }
+}
+
+public class QueryObserver<TResult> : QueryObserver<Unit, TResult>
+{
+    public QueryObserver(
+        Func<Task<TResult>> queryFn,
+        QueryObserverOptions<TResult>? options = null) : base(_ => queryFn(), options)
+    {
+        SetParams(default);
     }
 }
