e23ad32a89739c1497a8c9477c3717fea98de8b5
Jcparkyn <51850908+Jcparkyn@users.noreply.github.com>
2022-11-16 21:51:08 +1100

    Add basic bUnit test project

---
+++ test/Phetch.Blazor.Tests/BlazorTestHelpers.cs
@@ -0,0 +1,27 @@
+﻿namespace Phetch.Blazor.Tests;
+
+using System;
+using System.Collections.Generic;
+
+internal class BlazorTestHelpers
+{
+    public static (Action<T>, List<T>) MakeMonitoredAction<T>()
+    {
+        var actionCalls = new List<T>();
+        var action = (T arg) =>
+        {
+            actionCalls.Add(arg);
+        };
+        return (action, actionCalls);
+    }
+
+    public static (Action, List<object?>) MakeMonitoredAction()
+    {
+        var actionCalls = new List<object?>();
+        var action = () =>
+        {
+            actionCalls.Add(null);
+        };
+        return (action, actionCalls);
+    }
+}
