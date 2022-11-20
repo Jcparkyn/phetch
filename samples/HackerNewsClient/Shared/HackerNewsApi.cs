1c21147af2e8575d83b405a414b87f7eb932a1da
Jcparkyn <51850908+Jcparkyn@users.noreply.github.com>
2022-11-20 20:33:53 +1100

    Squashed 21 commits: Various changes to hacker news demo project
    hn: Make HN demo content wider on large screens
    
    hn: Defer comment rendering
    
    hn: Improve "Load More" button on search page
    
    hn: Replace Virtualize with custom DeferList component
    
    hn: Add simple "Ask HN" page
    
    hn: Add time display component
    
    hn: Move comment to its own component
    
    hn: Add simple preload for front page data
    
    hn: Add user page
    
    hn: Expand API to separate methods
    
    hn: Add Show page and various other improvements
    
    hn: Redesign comment component
    
    hn: Implement fancy reply colours and "show parents"
    
    hn: Implement proper comment counts
    
    hn: Add extra details to PostDetails page
    
    hn: Fancy comment collapsing
    
    hn: Scroll to comment synchronously
    
    hn: Add user post filtering
    
    hn: Implement better domain links
    
    hn: Hide comments with no text, and fix issues reported by Lighthouse
    
    hn: Add reply link to comments

--- samples/HackerNewsClient/Shared/HackerNewsApi.cs
+++ samples/HackerNewsClient/Shared/HackerNewsApi.cs
@@ -4,52 +4,78 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.WebUtilities;
 using Phetch.Core;
 
 public class HackerNewsApi
 {
+    private readonly HttpClient _httpClient;
+
     public HackerNewsApi(HttpClient httpClient)
     {
-        GetItem = new(
-            async (itemId, ct) => (await httpClient.GetFromJsonAsync<HnItemDetails>(
-                $"https://hn.algolia.com/api/v1/items/{itemId}",
+        _httpClient = httpClient;
+
+        var defaultOptions = new EndpointOptions()
+        {
+            DefaultStaleTime = TimeSpan.FromSeconds(60),
+            RetryHandler = RetryHandler.Simple(1),
+            OnFailure = e => Console.WriteLine(e.Exception.StackTrace),
+        };
+
+        GetItem = new(GetItemMethod, defaultOptions);
+
+        GetTopStories = new(GetTopStoriesMethod, defaultOptions);
+
+        GetUser = new(
+            async (userId, ct) => (await httpClient.GetFromJsonAsync<HnUser>(
+                $"https://hn.algolia.com/api/v1/users/{userId}",
                 ct
             ))!,
-            options: new()
-            {
-                DefaultStaleTime = TimeSpan.FromSeconds(60),
-                RetryHandler = RetryHandler.Simple(1),
-            }
-        );
-
-        GetTopStories = new(
-            async (args, ct) =>
-            {
-                var url = "https://hn.algolia.com/api/v1/search" +
-                    $"?tags={WebUtility.UrlEncode(args.Tag)}" +
-                    $"&query={WebUtility.UrlEncode(args.Query)}" +
-                    $"&hitsPerPage={args.PageSize}&page={args.Page}";
-                if (args.StartDate is DateTimeOffset dto)
-                {
-                    url += $"&numericFilters=created_at_i>{dto.ToUnixTimeSeconds()}";
-                }
-                var result = await httpClient.GetFromJsonAsync<SearchResponse<HnItem>>(url, ct);
-                return result!;
-            },
-            options: new()
-            {
-                DefaultStaleTime = TimeSpan.FromMinutes(2),
-            }
+            options: defaultOptions
         );
     }
 
-    public Endpoint<GetTopStoriesArgs, SearchResponse<HnItem>> GetTopStories { get; }
+    public async Task<HnItemDetails> GetItemMethod(int itemId, CancellationToken ct)
+    {
+        return (await _httpClient.GetFromJsonAsync<HnItemDetails>(
+            $"https://hn.algolia.com/api/v1/items/{itemId}",
+            ct
+        ))!;
+    }
+
+    public async Task<SearchResponse<HnItem>> GetTopStoriesMethod(SearchStoriesArgs args, CancellationToken ct)
+    {
+        var queries = new Dictionary<string, string?>()
+        {
+            { "tags", args.Tag },
+            { "query", args.Query },
+            { "hitsPerPage", args.PageSize.ToString() },
+            { "page", args.Page.ToString() },
+        };
+
+        if (args.StartDate is DateTimeOffset dto)
+            queries.Add("numericFilters", $"created_at_i>{dto.ToUnixTimeSeconds()}");
+
+        var url = QueryHelpers.AddQueryString("https://hn.algolia.com/api/v1/search", queries);
+
+        var result = await _httpClient.GetFromJsonAsync<SearchResponse<HnItem>>(url, ct);
+        return result!;
+    }
+
+    public Endpoint<SearchStoriesArgs, SearchResponse<HnItem>> GetTopStories { get; }
 
     public Endpoint<int, HnItemDetails> GetItem { get; }
 
-    public record GetTopStoriesArgs(int Page, int PageSize = 20, string Query = "", string Tag = "", DateTimeOffset? StartDate = null)
+    public Endpoint<string, HnUser> GetUser { get; }
+
+    public record SearchStoriesArgs(
+        int Page,
+        int PageSize = 20,
+        string? Query = "",
+        string? Tag = "",
+        DateTimeOffset? StartDate = null)
     {
-        public GetTopStoriesArgs GetNextPageArgs() => this with { Page = Page + 1 };
+        public SearchStoriesArgs GetNextPageArgs() => this with { Page = Page + 1 };
     }
 }
 
@@ -60,9 +86,15 @@ public record SearchResponse<T>(
     int NbPages
 );
 
+public record HnUser(
+    string Username,
+    string About,
+    int Karma
+);
+
 public record HnItem(
     [property: JsonPropertyName("objectID")] int Id,
-    [property: JsonPropertyName("created_at")] DateTime CreatedAt,
+    [property: JsonPropertyName("created_at")] DateTimeOffset CreatedAt,
     string Title,
     string? Url,
     string Author,
@@ -76,10 +108,10 @@ public record HnItem(
 
 public record HnItemDetails(
     int Id,
-    [property: JsonPropertyName("created_at")] DateTime CreatedAt,
+    [property: JsonPropertyName("created_at")] DateTimeOffset CreatedAt,
     string Title,
     string? Url,
-    string Author,
+    string? Author,
     int? Points,
     string? Text,
     string Type,
@@ -87,5 +119,11 @@ public record HnItemDetails(
 )
 {
     public bool HasChildren => Children.Count > 0;
+
+    public IEnumerable<HnItemDetails> ValidChilden { get; } = Children.Where(c => !string.IsNullOrEmpty(c.Text));
+
+    private int? _totalChildCount;
+
+    public int TotalChildCount => _totalChildCount ??= Children.Sum(static c => c.TotalChildCount) + Children.Count;
 };
 
---
+++ samples/HackerNewsClient/Shared/QueryParam.cs
@@ -0,0 +1,62 @@
+﻿namespace HackerNewsClient.Shared;
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+
+public sealed class QueryParam
+{
+    private string _value;
+    private readonly NavigationManager _navManager;
+    private readonly string _name;
+
+    public QueryParam(NavigationManager navManager, string name, string defaultValue)
+    {
+        _navManager = navManager;
+        _name = name;
+        var uri = navManager.ToAbsoluteUri(navManager.Uri);
+        _value = QueryHelpers.ParseQuery(uri.Query).TryGetValue(name, out var val)
+            ? val.ToString()
+            : defaultValue;
+    }
+
+    public string Value
+    {
+        get => _value;
+        set
+        {
+            _value = value;
+            _navManager.NavigateTo(_navManager.GetUriWithQueryParameter(_name, value), replace: true);
+        }
+    }
+}
+
+public sealed class QueryParam<T> where T : IParsable<T>, IFormattable
+{
+    private T _value;
+    private readonly NavigationManager _navManager;
+    private readonly string _name;
+
+    public QueryParam(NavigationManager navManager, string name, T defaultValue)
+    {
+        _navManager = navManager;
+        _name = name;
+        var uri = navManager.ToAbsoluteUri(navManager.Uri);
+        _value = QueryHelpers.ParseQuery(uri.Query).TryGetValue(name, out var val)
+            && T.TryParse(val.ToString(), CultureInfo.InvariantCulture, out var parsed)
+                ? parsed
+                : defaultValue;
+    }
+
+    public T Value
+    {
+        get => _value;
+        set
+        {
+            _value = value;
+            var stringValue = value.ToString(null, CultureInfo.InvariantCulture);
+            _navManager.NavigateTo(_navManager.GetUriWithQueryParameter(_name, stringValue), replace: true);
+        }
+    }
+}
