10a9a270935d59d3b3987e4eb9efd42ea7082968
Jcparkyn <51850908+Jcparkyn@users.noreply.github.com>
2023-11-18 18:04:54 +1100

    Add tests for UseParameterlessEndpoint

---
+++ test/Phetch.Blazor.Tests/MockQueryFn.cs
@@ -0,0 +1,43 @@
+﻿namespace Phetch.Blazor.Tests;
+
+using Phetch.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Class for creating query functions for testing.
+/// </summary>
+/// <param name="numSources">
+/// Number of TaskCompletionSources to create, which is usually equal to the number of times this
+/// function will be called.
+/// </param>
+public class MockQueryFn<TArg, TResult>(int numSources)
+{
+    public List<TaskCompletionSource<TResult>> Sources { get; } = Enumerable.Range(0, numSources)
+        .Select(_ => new TaskCompletionSource<TResult>())
+        .ToList();
+
+    /// <summary>
+    /// List of calls that have been made to this query function.
+    /// </summary>
+    public List<TArg> Calls { get; } = [];
+
+    private int _queryCount = 0;
+
+    public async Task<TResult> Query(TArg arg)
+    {
+        if (_queryCount > Sources.Count)
+            throw new Exception("Query function called too many times");
+        var resultTask = Sources[_queryCount].Task;
+        _queryCount++;
+        Calls.Add(arg);
+        return await resultTask;
+    }
+}
+
+public class MockParameterlessQueryFn<TResult>(int numSources) : MockQueryFn<Unit, TResult>(numSources)
+{
+    public async Task<TResult> Query() => await Query(default);
+}
