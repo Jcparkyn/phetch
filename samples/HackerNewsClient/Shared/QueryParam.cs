1cbaf85522e120287df3e96e38b6ad487fc20d30
Jcparkyn <51850908+Jcparkyn@users.noreply.github.com>
2023-11-23 20:22:33 +1100

    Simplify QueryParam class

--- samples/HackerNewsClient/Shared/QueryParam.cs
+++ samples/HackerNewsClient/Shared/QueryParam.cs
@@ -4,34 +4,7 @@ using System.Globalization;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.WebUtilities;
 
-public sealed class QueryParam
-{
-    private string _value;
-    private readonly NavigationManager _navManager;
-    private readonly string _name;
-
-    public QueryParam(NavigationManager navManager, string name, string defaultValue)
-    {
-        _navManager = navManager;
-        _name = name;
-        var uri = navManager.ToAbsoluteUri(navManager.Uri);
-        _value = QueryHelpers.ParseQuery(uri.Query).TryGetValue(name, out var val)
-            ? val.ToString()
-            : defaultValue;
-    }
-
-    public string Value
-    {
-        get => _value;
-        set
-        {
-            _value = value;
-            _navManager.NavigateTo(_navManager.GetUriWithQueryParameter(_name, value), replace: true);
-        }
-    }
-}
-
-public sealed class QueryParam<T> where T : IParsable<T>, IFormattable
+public sealed class QueryParam<T> where T : IParsable<T>
 {
     private T _value;
     private readonly NavigationManager _navManager;
@@ -54,7 +27,11 @@ public sealed class QueryParam<T> where T : IParsable<T>, IFormattable
         set
         {
             _value = value;
-            var stringValue = value.ToString(null, CultureInfo.InvariantCulture);
+            var stringValue = value switch
+            {
+                IFormattable formattable => formattable.ToString(null, CultureInfo.InvariantCulture),
+                _ => value.ToString(),
+            };
             _navManager.NavigateTo(_navManager.GetUriWithQueryParameter(_name, stringValue), replace: true);
         }
     }
